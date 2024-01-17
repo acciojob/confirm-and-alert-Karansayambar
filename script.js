04d27943f12932f5b6744b60540fd15e36290e22
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-17 20:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Asking for confirmation using confirm
+const userConfirmation = confirm("Do you want to proceed?");
+
+// Checking the user's response
+if (userConfirmation) {
+  // Displaying an alert if OK is clicked
+  alert("You clicked OK. Proceeding...");
+} else {
+  // Displaying an alert if Cancel is clicked
+  alert("You clicked Cancel. Exiting...");
+}
